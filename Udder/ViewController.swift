8625603642e2cf91e7b795b57dd53950ed75ce0b
sing <526117922@qq.com>
2020-06-26 17:11:11 +0800

    Initial Commit

---
+++ Udder/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Udder
+//
+//  Created by 王剑豪 on 2020/6/26.
+//  Copyright © 2020 Sing. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
